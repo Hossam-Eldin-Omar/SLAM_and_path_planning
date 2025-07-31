537542bf0eb2a6dfa25ac6ce549c453fc6f59acb
Hossam-Eldin-Omar <167653916+Hossam-Eldin-Omar@users.noreply.github.com>
2025-07-31 15:23:00 +0300

    Delete build directory

--- build/turtlebot3_fake_node/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/hossam-omar/nav2_ws/src/turtlebot3_fake_node
-# Build directory: /home/hossam-omar/nav2_ws/build/turtlebot3_fake_node
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
